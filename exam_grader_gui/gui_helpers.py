3391d71398c8c60f6ea498685e36500531ecb2dc
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2023-02-28 12:21:13 +0100

    First UI draft

---
+++ exam_grader_gui/gui_helpers.py
@@ -0,0 +1,140 @@
+import os
+import subprocess
+import sys
+from typing import Optional
+from collections import deque
+from datetime import datetime
+from pathlib import Path
+
+from gi.repository import Gdk, Gtk
+
+provider = Gtk.CssProvider()
+provider.load_from_data(
+    """
+    #failable_entry.red { background: LightCoral; }
+    """.encode()
+)
+
+
+def get_content(entry, target_type):
+    """
+    Helper fn that queries an gtk entry and colors the entry box upon invalid content
+    Returns the content cast into `desired_type`
+    """
+    Gtk.StyleContext.add_provider_for_screen(
+        Gdk.Screen.get_default(), provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
+    text = entry.get_text()
+    entry.set_name("failable_entry")
+    if text == "":
+        entry.get_style_context().remove_class("red")
+        return None
+    try:
+        ret = target_type(text)
+        entry.get_style_context().remove_class("red")
+        return ret
+    except ValueError:
+        # print("Invalid entry: " + text)
+        entry.get_style_context().add_class("red")
+        return None
+
+
+def get_content_list(entry, target_type):
+    """
+    Helper fn that queries an gtk entry and colors the entry box upon invalid content
+    Returns the content as a list splitted at ',' and cast into `desired_type`
+    """
+    Gtk.StyleContext.add_provider_for_screen(
+        Gdk.Screen.get_default(), provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
+    text = entry.get_text()
+    entry.set_name("failable_entry")
+    if text == "":
+        entry.get_style_context().remove_class("red")
+        return None
+    try:
+        content_list = list(map(target_type, text.strip(",").split(",")))
+        entry.get_style_context().remove_class("red")
+        return content_list
+    except ValueError:
+        entry.get_style_context().add_class("red")
+        return None
+
+
+def show_about_dialog(parent):
+    gladefile = Path(__file__) / "../glade/About.glade"
+    builder = Gtk.Builder()
+    builder.add_from_file(str(gladefile.resolve()))
+    dialog = builder.get_object("about")
+    dialog.set_transient_for(parent)
+    dialog.run()
+    dialog.destroy()
+
+
+def open_file(filename):
+    """
+    opens a file with the default handler.
+    Kudos to https://stackoverflow.com/a/17317468/6551168
+    """
+    if sys.platform == "win32":
+        os.startfile(filename)
+    else:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
+
+
+def successful_with_open_folder_dialog(parent_window, message: str, path: Path):
+    dialog = Gtk.MessageDialog(
+        parent_window,
+        Gtk.DialogFlags.MODAL
+        | Gtk.DialogFlags.DESTROY_WITH_PARENT
+        | Gtk.DialogFlags.USE_HEADER_BAR,
+        Gtk.MessageType.INFO,
+        Gtk.ButtonsType.OK,
+        message,
+    )
+    dialog.add_button("Open Folder", 42)
+    dialog.show_all()
+    response = dialog.run()
+    if response == 42:
+        open_file(path)
+    dialog.destroy()
+
+
+class ETA_calculator:
+    def __init__(self, nr_elements: int, smooting_elems: int = 8):
+        self.start = datetime.now()
+        self.nr_elements: int = nr_elements
+        self.etas: deque = deque(maxlen=smooting_elems)
+        self.progress: int = 0
+        self.avg_eta: Optional[int] = None
+        self.smooting_elems = smooting_elems
+
+    def update(self):
+        self.progress += 1
+        time_passed = datetime.now() - self.start
+        eta = (self.nr_elements / self.progress - 1) * time_passed
+        self.etas.append(eta.seconds)
+        self.avg_eta = int(sum(self.etas) / len(self.etas)) + 1
+
+    def get_eta_seconds(self) -> Optional[int]:
+        return self.avg_eta
+
+    def get_eta_text(self) -> str:
+        if self.avg_eta is None:
+            return "ETA: -"
+        elif self.avg_eta < 30:
+            return f"ETA: {self.avg_eta}s"
+        elif self.avg_eta < 90:
+            rounded_eta = int((self.avg_eta - 1) / 5 + 1) * 5
+            return f"ETA: {rounded_eta}s"
+        elif self.avg_eta < 60 * 15:
+            minutes = int((self.avg_eta + 14) / 60)
+            seconds = self.avg_eta - (minutes * 60)
+            rounded_seconds = int((seconds - 1) / 15 + 1) * 15  # Round towards 15 s
+            return f"ETA: {minutes}min {rounded_seconds}s"
+        else:
+            minutes = int(self.avg_eta / 60)
+            return f"ETA: {minutes}min"
--- exam_grader_gui/main_window.py
+++ exam_grader_gui/main_window.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from logging import error
 from pathlib import Path
+from typing import Callable, List
 
 from gi.repository import Gtk
 
@@ -21,12 +22,6 @@ class MainWindow:
         self.window = self.builder.get_object("window")
 
         self.main_stack = self.builder.get_object("main_stack")
-        # self.barcode_generation_box = BarcodeGeneratorBox(self)
-        # self.main_stack.add_titled(
-        #     self.barcode_generation_box, "barcode_generation", "Barcode Generation"
-        # )
-        # self.scan_sorter = ScanSorter(self)
-        # self.main_stack.add_titled(self.scan_sorter, "scan_sorter", "Scan Sorting")
 
         self.window.show_all()
         handlers = {
@@ -35,15 +30,21 @@ class MainWindow:
             "on_about_clicked": self.on_about_clicked,
             "export_clicked": self.export,
             "on_main_stack_visible_child_changed": self.on_main_stack_visible_child_changed,
+            "on_examdate_clicked": self.on_examdate_clicked,
         }
         self.builder.connect_signals(handlers)
 
         self.help_menu_popover = self.builder.get_object("help_menu_popover")
 
+        self.task_list = self.builder.get_object("task_list")
         self.export_button = self.builder.get_object("export_button")
         self.open_button = self.builder.get_object("open_button")
 
+        self.task_list.add(AddTaskRow(self.add_task))
+
         self.set_visible_buttons(GuiPages.EXAM_SETUP)
+
+        self.tasks: List[str, int] = []
 
     def on_main_stack_visible_child_changed(self, stack_widget, variable):
         visible_page = stack_widget.get_visible_child_name()
@@ -70,5 +71,65 @@ class MainWindow:
     def open(self, _widget):
         pass
 
+    def on_examdate_clicked(self, widget):
+        print("examdate selected")
+
     def export(self, _widget):
         pass
+
+    def add_task(self, name: str, points: int):
+        self.tasks.append((name, points))
+        self.task_list.remove(self.task_list.get_children()[-1])
+        self.task_list.add(TaskRow(len(self.tasks) - 1, name, points, self.remove_task))
+        self.task_list.add(AddTaskRow(self.add_task))
+        self.task_list.show_all()
+
+    def remove_task(self, id: int):
+        # First two items are header and seperator
+        self.task_list.remove(self.task_list.get_children()[id + 2])
+        self.tasks.remove(self.tasks[id])
+        self.task_list.show_all()
+
+
+@Gtk.Template(filename=str((Path(__file__) / "../glade/Add_Task_Row.glade").resolve()))
+class AddTaskRow(Gtk.Box):
+    __gtype_name__ = "add_task_row"
+
+    taskname_entry = Gtk.Template.Child("taskname_entry")
+    points_entry = Gtk.Template.Child("points_entry")
+    add_button = Gtk.Template.Child("add_button")
+
+    def __init__(self, cb: Callable[[str, int], None]):
+        super(Gtk.Box, self).__init__()
+
+        self.add_button.connect("clicked", self.add_clicked)
+        self.cb = cb
+
+    def add_clicked(self, widget):
+        try:
+            points = int(self.points_entry.get_text())
+            self.cb(self.taskname_entry.get_text(), points)
+        except ValueError:
+            # TODO: mark points red
+            pass
+
+
+@Gtk.Template(filename=str((Path(__file__) / "../glade/Task_Row.glade").resolve()))
+class TaskRow(Gtk.Box):
+    __gtype_name__ = "task_row"
+
+    taskname_label = Gtk.Template.Child("taskname_label")
+    points_label = Gtk.Template.Child("points_label")
+    remove_button = Gtk.Template.Child("remove_button")
+
+    def __init__(self, id: int, name: str, points: int, cb: Callable[[int], None]):
+        super(Gtk.Box, self).__init__()
+
+        self.id = id
+        self.taskname_label.set_text(name)
+        self.points_label.set_text(str(points))
+        self.remove_button.connect("clicked", self.remove_clicked)
+        self.cb = cb
+
+    def remove_clicked(self, widget):
+        self.cb(self.id)
